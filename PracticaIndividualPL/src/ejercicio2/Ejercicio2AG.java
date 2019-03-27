6b2efce0908df8dccdd61bc77368caad74ab2461
Alejandro Fuentes Gómez <alefuegom@alum.us.es>
2019-03-27 13:16:34 +0100

    PRACTICA INDIVIDUAL 3

--- PracticaIndividualPL/src/ejercicio2/Ejercicio2AG.java
+++ PracticaIndividualPL/src/ejercicio2/Ejercicio2AG.java
@@ -3,6 +3,7 @@ package ejercicio2;
 
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import org.jgrapht.Graph;
@@ -11,6 +12,8 @@ import org.jgrapht.Graph;
 import tipos.*;
 import us.lsi.ag.IndexChromosome;
 import us.lsi.ag.IndexProblemAG;
+import us.lsi.ag.ValuesInRangeChromosome;
+import us.lsi.ag.ValuesInRangeProblemAG;
 import us.lsi.ag.agchromosomes.BinaryChromosome;
 
 
@@ -22,7 +25,7 @@ public class Ejercicio2AG implements IndexProblemAG<List<Tarea>> {
 	private  Integer procesadores;
 	
 	
-	public Ejercicio2AG(List<Integer> Tareaes, Integer procesadores, List<Tarea> tareas) {
+	public Ejercicio2AG(List<Tarea> tareas, Integer procesadores) {
 		
 		this.tareas = tareas;
 		this.procesadores = procesadores;
@@ -34,39 +37,88 @@ public class Ejercicio2AG implements IndexProblemAG<List<Tarea>> {
 		return tareas.size();
 	}
 
+	
+	private Double restriccionDuracionTareas(List<List<Integer>> duracionProcesadores, Double duracion) {
+		Double res = 0.;
+		Integer aux= 0;
+		for(List<Integer> i : duracionProcesadores) {
+			for(int j = 0; j<i.size(); j++) {
+				aux = aux + i.get(j);
+	
+			}
+			if(aux>duracion) {
+				res = duracion;
+			}
+		}
+		return res;
+		
+		
+	}
+	
+	private Double restriccionNumeroProcesador(List<Tarea> tareas) {
+		Double res = 0.;
+		for(Tarea t : tareas) {
+			if(t.getProcesador().size()>1) {
+				res = res + t.getProcesador().size();
+			}
+			
+		}
+		return res;
+	}
 
-	@Override
+
+
+	
 	public Double fitnessFunction(IndexChromosome cr) {
 		// TODO Auto-generated method stub
-		Double k = 0.0;
+		Double k = 1000000.0;
 		Double res = 0.0;
-		Integer duracion = 0;
+		Double duracion = 0.;
+		List<List<Integer>> duracionProcesadores = new ArrayList<List<Integer>>();
+		List<Integer> duracionProcesador = new ArrayList<Integer>();
 		List<Tarea> l = getSolucion(cr);
-		
-		for(int i=0;i<l.size();i++) {
-		 for(int j= 0; i<procesadores;j++) {
-		Tarea t = l.get(i);
-		//TODO
-			 
-		 } 
+	
+		for(Tarea t: l) {
+			Integer aux = t.getDuracion();
+			Integer p = t.getProcesador().get(0);
+			duracionProcesador.add(aux);
+			duracionProcesadores.add(p, duracionProcesador);
+			duracion = duracion +aux;
 		 }
+		Integer max = 0;
+		Integer aux = 0;
+		for(int i = 0;i<duracionProcesadores.size(); i++) {
+			
+			for(int j = 0; j<duracionProcesador.size();j++) {
+				aux = aux + duracionProcesador.get(j);	
+			}
+			if(aux>max) {
+				max = aux;
+			}	
+		}
+		
+		res = duracion - max;
+		
+		
+		res = -res -k*(Math.pow((restriccionDuracionTareas(duracionProcesadores, duracion)), 2)+
+				Math.pow(restriccionNumeroProcesador(l), 2));
+		
 		
 		
 	return res;	
 	}
-
-
-	@Override
+	
 	public List<Tarea> getSolucion(IndexChromosome cr) {
 		List<Integer> cromo = cr.decode();
 		List<Tarea> res = new ArrayList<Tarea>();
 		
 		for (int i = 0; i < cromo.size(); i++) {
-			res.add(tareas.get(cromo.get(i)));
+			Integer aux = cromo.get(i);
+			tareas.get(i).getProcesador().add(aux);
+			res.add(tareas.get(i));
 		}
 		return res;
 	}
-	
 	
 	
 	
--- PracticaIndividualPL/src/ejercicio2/Ejercicio2AG_Test.java
+++ PracticaIndividualPL/src/ejercicio2/Ejercicio2AG_Test.java
@@ -1,5 +1,6 @@
 package ejercicio2;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import ejercicio2.Ejercicio2AG;
@@ -10,15 +11,51 @@ import us.lsi.ag.agchromosomes.AlgoritmoAG;
 import us.lsi.ag.agchromosomes.ChromosomeFactory.ChromosomeType;
 import us.lsi.ag.agstopping.StoppingConditionFactory;
 import us.lsi.ag.agstopping.StoppingConditionFactory.StoppingConditionType;
+import us.lsi.ag.anuncios.ProblemaAnunciosAG;
+import us.lsi.anuncios.datos.DatosAnuncios;
+import us.lsi.anuncios.datos.ListaDeAnunciosAEmitir;
 
 public class Ejercicio2AG_Test {
 
 	public static void main(String[] args) {
 
 		getConditions();
+		Tarea t0 = new Tarea(5, new ArrayList<Integer>());
+		Tarea t1 = new Tarea(4, new ArrayList<Integer>());
+		Tarea t2 = new Tarea(6, new ArrayList<Integer>());
+		Tarea t3 = new Tarea(3, new ArrayList<Integer>());
+		Tarea t4 = new Tarea(2, new ArrayList<Integer>());
 		
+		List<Tarea> tareas = new ArrayList<Tarea>();
+		tareas.add(t0);
+		tareas.add(t1);
+		tareas.add(t2);
+		tareas.add(t3);
+		tareas.add(t4);
+		
+		imprimeSolucion(tareas,1);
 	}
 	
+	private static void imprimeSolucion(List<Tarea> tareas, int i) {
+		IndexProblemAG<List<Tarea>> p = new Ejercicio2AG(tareas,i);
+		AlgoritmoAG<IndexChromosome> alg = AlgoritmoAG.create(ChromosomeType.IndexPermutation, p);
+		
+		IndexChromosome sol = alg.getBestChromosome();
+		List<Tarea> problemaSol = p.getSolucion(sol);
+		
+		System.out.println("Solucion : "+problemaSol );
+		
+		for(int j= 0;j<problemaSol.size();j++) {
+			
+			System.out.println("Tarea "+j+ "con duracion "+ problemaSol.get(j).getDuracion() +
+					"asignada al procesador "+problemaSol.get(j).getProcesador().get(0));
+			
+		}
+		
+		
+		
+	}
+
 	private static void getConditions() {
 		AlgoritmoAG.ELITISM_RATE = 0.2;
 		AlgoritmoAG.CROSSOVER_RATE = 0.8;
